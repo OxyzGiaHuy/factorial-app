519c63cf4ca7eaa074cc5d49e6dc190fae355589
OxyzGiaHuy <108162560+OxyzGiaHuy@users.noreply.github.com>
2024-03-10 20:35:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ import streamlit as st
 from factorial import factorial
 
 def main():
-    st.title("Factorial Calculator")
-    number = st.number_input("Enter a number: ", 
+    st.title("AIO 2024 - Factorial Calculator")
+    number = st.number_input("Enter a number (from 0 to 900): ", 
                              min_value=0,
                              max_value=900)
     if st.button("Calculate"):
         result = factorial(number)
-        st.write(f"The factorial of {number} is {result}")
-    st.balloons()
+        st.write(f"The factorial of {number} is {result}.")
+        st.balloons()
 
 if __name__ == "__main__":
     main()
